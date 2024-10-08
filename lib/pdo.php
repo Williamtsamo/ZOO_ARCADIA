9b564287459ad1f989eb0d6c16cccf46ee4dfe3e
william tsamo <williamtsamo@yahoo.fr>
2024-10-08 18:50:03 +0200

    connexion jawsdb

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,13 +1,42 @@
 <?php
 
-try {
-    $conn = new PDO('mysql:dbname=arcadia;host=localhost:3307;charset=utf8', "root", "");
-    // set the PDO error mode to exception
-    //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected";
-} catch (PDOException $e) {
 
+
+//try {
+//    $conn = new PDO('mysql:dbname=arcadia;host=localhost:3307;charset=utf8', "root", "");
+// set the PDO error mode to exception
+//$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//echo "Connected";
+//} catch (PDOException $e) {
+
+//   die('Erreur : ' . $e->getMessage());
+//}
+
+
+try {
+    if (getenv('JAWSDB_URL') !== false) {
+        $dbparts = parse_url(getenv('JAWSDB_URL'));
+
+        // Connexion à JawsDB via PDO
+        $hostname = $dbparts['mfplywfpf75qwuaz@gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
+        $username = $dbparts['e00pkwpgqc7iz7dk'];
+        $password = $dbparts['mfplywfpf75qwuaz'];
+        $database = ltrim($dbparts['nvpri7rto4mc24hf'], '/');
+
+
+        // Création de la connexion PDO avec JawsDB
+        $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+        echo "Connected to JawsDB";
+    } else {
+        // Connexion à une base de données locale si JawsDB n'est pas disponible
+        $conn = new PDO('mysql:host=localhost:3307;dbname=arcadia;charset=utf8', 'root', '');
+        echo "Connected to local database";
+    }
+
+    // Configuration du mode d'erreur PDO
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Gestion de l'exception si une erreur de connexion survient
     die('Erreur : ' . $e->getMessage());
 }
-
 ?>
